c0d487a97e8048d80eb008cb8f7d3ba33557b97e
Pranav Bhatia <157147243+pranavb1234@users.noreply.github.com>
2026-07-15 01:11:37 +0530

    Correct syntax in addNumbers function
    Fix syntax errors in addNumbers function.

---
+++ t.js
@@ -0,0 +1,12 @@
+function addNumbers(a, b {
+    let result == a + b
+    if (result = 10) {
+        console.log("Result is 10")
+    else
+        console.log("Result is not 10");
+    }
+
+    return results;
+}
+
+console.log(addNumbers(5, 5));
